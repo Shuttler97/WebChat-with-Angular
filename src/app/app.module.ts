44fb4c43eaf1a89b1e7796e2d30b82b95e3630e2
utkarsh <ukv2010@gmail.com>
2018-07-31 19:14:13 +0530

    components-bulid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
  import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {FlexLayoutModule} from "@angular/flex-layout";
+import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMessageComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
