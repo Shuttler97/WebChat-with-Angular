f1510f8cb7512f42982823495506f66b5b84d86e
utkarsh <ukv2010@gmail.com>
2018-08-02 00:42:40 +0530

    designed-ChatModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
- import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+ import {MatButtonModule, MatCheckboxModule, MatInputModule, MatToolbarModule} from '@angular/material';
 import {FlexLayoutModule} from "@angular/flex-layout";
-import { ChatMessageComponent } from './chat-message/chat-message.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ChatInfoComponent } from './chat-info/chat-info.component';
+import {ChatsComponent} from "./chats/chats.component";
+import { ChatMessageComponent } from './chat-message/chat-message.component';
+import { MessageBarComponent } from './message-bar/message-bar.component';
+import {FormsModule} from "@angular/forms";
+import { ProfileHeaderComponent } from './profile-header/profile-header.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { UserRecentComponentComponent } from './user-recent-component/user-recent-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProfileComponent,
+    ChatInfoComponent,
+    ChatsComponent,
     ChatMessageComponent,
-    ProfileComponent
+    MessageBarComponent,
+    ProfileHeaderComponent,
+    SearchBarComponent,
+    UserRecentComponentComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    FormsModule,
+    MatInputModule
 
   ],
   providers: [],
---
+++ src/app/user-recent-component/user-recent-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-recent-component',
+  templateUrl: './user-recent-component.component.html',
+  styleUrls: ['./user-recent-component.component.css']
+})
+export class UserRecentComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
